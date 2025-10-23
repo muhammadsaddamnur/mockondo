e561199b577b346ec5c62338f33c3c139ce934f8
Muhammad Saddam Nur <muhammadsaddamnur@gmail.com>
2025-10-23 09:27:36 +0700

    fix: add early return in _getIpAddress to prevent errors when mockModels is empty

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -39,6 +39,8 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<void> _getIpAddress() async {
+    if (mockModels.isEmpty) return;
+
     final info = NetworkInfo();
     final wifiIP = await info.getWifiIP(); // IP lokal di jaringan Wi-Fi
 
